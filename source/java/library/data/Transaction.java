1d7dee8a4e411831809f7d7a58cb6ef75437a912
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2018-04-06 18:59:15 -0400

    Start working on transaction implementation

---
+++ source/java/library/data/Transaction.java
@@ -0,0 +1,80 @@
+package library.data;
+
+import java.time.Instant;
+
+/**
+ * Used to store all transactions that take place in a library instance.
+ * Stores the affected patron, affected book, the action that took place, and a timestamp of when it occurred.
+ * This object is identified by an {@link Identifier}.
+ */
+public class Transaction implements LibraryData {
+    private final Identifier identifier;
+    private final Patron changedPatron;
+    private final Book changedBook;
+    private final Action action;
+    private final Instant timestamp;
+
+    /**
+     * Used to create an instance of a Transaction object. Should be used for creating one for testing, or from user input
+     * Use {@link #Transaction(String[], Library)} for restoring from a saved instance of a Transaction
+     *
+     * @param identifier    The identifier of this transaction
+     * @param changedPatron The {@link Patron} affected by this transaction
+     * @param changedBook   The {@link Book} affected by this transaction
+     * @param action        The {@link Action} that took place in this transaction
+     * @param timestamp     The {@link Instant} that this transaction took place
+     */
+    public Transaction(Identifier identifier, Patron changedPatron, Book changedBook, Action action, Instant timestamp) {
+        this.identifier = identifier;
+        this.changedPatron = changedPatron;
+        this.changedBook = changedBook;
+        this.action = action;
+        this.timestamp = timestamp;
+    }
+
+    public Transaction(String[] data, Library library) {
+        if (data.length != 5) {
+            throw new RuntimeException("Invalid data array passed to create a Transaction object.");
+        }
+        identifier = new Identifier(data[0]);
+        changedPatron = library.getPatronFromID(new Identifier(data[1]));
+        changedBook = library.getBookFromID(new Identifier(data[2]));
+        action = Action.valueOf(data[3]);
+        timestamp = Instant.parse(data[4]);
+    }
+
+    @Override
+    public String[] asData() {
+        return new String[]{identifier.getId(),
+                changedPatron.getIdentifier().getId(),
+                changedBook.getIdentifier().getId(),
+                Action.CHECKOUT.name(),
+                timestamp.toString()};
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public Book getChangedBook() {
+        return changedBook;
+    }
+
+    public Patron getChangedPatron() {
+        return changedPatron;
+    }
+
+    @Override
+    public Identifier getIdentifier() {
+        return identifier;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    enum Action {
+        CHECKOUT,
+        RETURN
+    }
+}
